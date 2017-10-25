16e49c206e02cee3af2cd88cba4ddc9e35c4890e
trenshaw88 <trenshaw88@twc.com>
2017-10-25 01:09:58 -0400

    Deploying app

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(port, function() {
-  console.log('I am listening on port 8080!');
+  console.log('I am listening on port 8000!');
 });
